df4ea86a8938a3b3bb4ea46e1533b569b7e199c1
Naicker Creason <naickercreason@zipcodes-MacBook-Pro-7.local>
2021-10-30 11:59:26 -0400

    finally got it to work!

--- string_utils.py
+++ string_utils.py
@@ -1,30 +1,54 @@
-
-def str_len(str_in: str) -> str:
+def str_len(str_in: str) -> int:
     """
     Given a string parameter, this function should return the length of the parameter.
+    :type str_in: object
+    :rtype: object
     """
-    pass  # remove pass statement and implement me
+    string_length = len(str_in)
+    return string_length
+
+
+print(str_len('PYTHON'))
 
 
 def first_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the first letter of the parameter.
     """
-    pass  # remove pass statement and implement me
+    first_char_yo = str_in[0]
+    return first_char_yo
+
+
+print(first_char('python'))
 
 
 def last_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the last letter of the parameter..
     """
-    pass  # remove pass statement and implement me
+    length = len(str_in)
+
+    last_string_char = str_in[length - 1]
+
+    return last_string_char
+
+
+print(last_char('python'))
 
 
 def input_has_substring(str_in: str, sub_str_in: str) -> bool:
     """
     This function determines if the substring exists within the string. Returns True or False.
+    :type sub_str_in: object
     """
-    pass  # remove pass statement and implement me
+
+    if sub_str_in in str_in:
+        return True
+    else:
+        return False
+
+
+print(input_has_substring('python', 'p'))
 
 
 def substring(str_in: str, start: int, stop: int) -> str:
@@ -35,8 +59,14 @@ def substring(str_in: str, start: int, stop: int) -> str:
     str_in -- the string in which to generate a substring from
     start -- starting position of the input parameter to start the substring (inclusive)
     stop -- stopping position of the input parameter to stop the substring (exclusive)
+    :rtype: object
     """
-    pass  # remove pass statement and implement me
+    input = str_in
+    print(input[start:stop])
+
+
+
+substring("python is super cool", 0, 5)
 
 
 def opposite_case(str_in: str) -> str:
@@ -44,5 +74,10 @@ def opposite_case(str_in: str) -> str:
     Given a string parameter, this function returns the same string back with each letter having the opposite case.
     Example: 
     When input = "Python" the function returns "pYTHON"
+    :rtype: object
     """
-    pass  # remove pass statement and implement me
+
+    return str_in.swapcase()
+
+
+print(opposite_case("Python"))
