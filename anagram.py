7bf9f00329ef12e2c5aa54e726fe6b7354a74cf8
Naicker Creason <naickercreason@zipcodes-MacBook-Pro-7.local>
2021-10-31 13:02:57 -0400

    finished this one!

--- anagram.py
+++ anagram.py
@@ -2,5 +2,13 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    pass  # remove pass statement and implement me
+    x = ''.join(elem.lower() for elem in first_string if elem.isalpha())
+    y = ''.join(elem.lower() for elem in second_string if elem.isalpha())
 
+    if (sorted(x) == sorted(y)):
+        return True
+    else:
+        return False
+
+
+print(is_anagram("RACECAR", "rACEcar"))
